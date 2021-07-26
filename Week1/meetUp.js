b85b10b3bbc38b35c679ad0ed886557293fda06d
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-07-26 16:45:17 +0200

    test/create branch

---
+++ Week1/meetUp.js
@@ -0,0 +1,10 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'hyfuser',
+  password : 'hyfpassword',
+  database : 'meetup',
+  // port : 3307
+});
+
+connection.connect();
